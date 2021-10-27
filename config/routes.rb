d6e8d3221bf887ed3a66120b0858a28e0439aa42
Daniel Buriticá <dani.ferbu@gmail.com>
2021-10-27 22:37:21 +0000

    Create Post model and generate User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'home', to: 'pages/home'
-  get 'profile', to: 'pages/profile'
-  get 'feed', to: 'pages/feed'
+  root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'profile', to: 'pages#profile'
+  get 'feed', to: 'pages#feed'
   devise_for :users
 end
