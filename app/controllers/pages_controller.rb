cf9c027b8bdd41df57bc720cea1b29214632b10d
Daniel Buriticá <dani.ferbu@gmail.com>
2021-11-01 03:16:55 +0000

    New feature,mark posts as seen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @post = current_user.posts.build
-      @feed_items = Post.all.paginate(page: params[:page])
+      @feed_items = Post.left_outer_joins(:seenPosts).where(seenPosts: {post_id: nil}).paginate(page: params[:page])
       Post.where('expirationDate < ?', Time.zone.now).delete_all
     end
   end
